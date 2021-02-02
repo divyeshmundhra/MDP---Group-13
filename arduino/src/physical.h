794c69a38f0bbffe6385c281bca3a9de25285615
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-02-02 17:43:30 +0800

    Fudge circumference calculation to make rotations more accurate

--- arduino/src/physical.h
+++ arduino/src/physical.h
@@ -8,7 +8,7 @@ const uint16_t kCount_per_rev = 2249;
 const uint16_t kWheel_diameter = 60;
 
 // center-to-center distance (mm) between the two wheels
-const uint16_t kWheel_distance = 174;
+const uint16_t kWheel_distance = 175;
 
 const uint16_t kWheel_circumference = 3.14159 * kWheel_diameter;
 const uint16_t kRobot_circumference = 3.14159 * kWheel_distance;
