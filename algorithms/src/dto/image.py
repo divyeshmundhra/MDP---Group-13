a6f6badaae6b15e8a250cfbd78f0b22e0295646d
Jay <milkweedz@hotmail.com>
2021-02-05 15:29:12 +0800

    bug: image.py missing image id getter

--- algorithms/src/dto/image.py
+++ algorithms/src/dto/image.py
@@ -1,12 +1,16 @@
 from src.dto.constants import Orientation
+from src.dto import Coord
 
 class Image:
-    def __init__(self, image_id, coord, orientation):
+    def __init__(self, image_id: int, coord: Coord, orientation: Orientation):
         self.image_id = image_id
         self.coord = coord
         self.orientation = orientation
 
-    def get_coord(self):
+    def get_image_id(self) -> int:
+        return self.image_id
+
+    def get_coord(self) -> Coord:
         return self.coord
 
     def get_orientation(self) -> Orientation:
