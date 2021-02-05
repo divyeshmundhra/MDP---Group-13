905acf63a077691dd0b469fad923f12a8d3963f8
Jay <milkweedz@hotmail.com>
2021-02-05 15:27:01 +0800

    bug: robot_info missing setter

--- algorithms/src/dto/robot_info.py
+++ algorithms/src/dto/robot_info.py
@@ -1,15 +1,19 @@
 from src.dto.constants import Orientation
+from src.dto import Coord
 
 class Robotinfo:
     def __init__(self, coord, orientation):
         self.coord = coord
         self.orientation = orientation
 
-    def get_coord(self):
+    def get_coord(self) -> Coord:
         return self.coord
     
-    def set_coord(self, coord):
+    def set_coord(self, coord: Coord) -> None:
         self.coord = coord
 
     def get_orientation(self) -> Orientation:
         return self.orientation
+
+    def set_orientation(self, orientation: Orientation) -> None:
+        self.orientation = orientation
