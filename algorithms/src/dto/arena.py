cf6e31f0367b3e4b13a83d3ff955146e49eff80d
Jay <milkweedz@hotmail.com>
2021-02-05 10:13:56 +0800

    add arena, cell, coord classes

---
+++ algorithms/src/dto/arena.py
@@ -0,0 +1,9 @@
+class Arena:
+    def __init__(self):
+        self.cell_matrix = [[None for y in range(20)] for x in range(15)]
+    
+    def set_cell_at_coord(self, cell, coord):
+        self.cell_matrix[coord.x][coord.y] = cell
+
+    def get_cell_at_coord(self, coord):
+        return self.cell_matrix[coord.x][coord.y]
