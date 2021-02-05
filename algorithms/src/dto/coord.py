faeb4f5236db86085e1f53ea3f2617d01179aec8
Jay <milkweedz@hotmail.com>
2021-02-05 11:05:10 +0800

    add getters and setters to arena and its dependencies

--- algorithms/src/dto/coord.py
+++ algorithms/src/dto/coord.py
@@ -2,3 +2,15 @@ class Coord:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def get_x(self):
+        return self.x
+    
+    def set_x(self, x):
+        self.x = x
+    
+    def get_y(self):
+        return self.y
+    
+    def set_y(self, y):
+        self.y = y
