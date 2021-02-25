c4babbefcd1317f5097accbec0a3d542d0877841
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-02-25 17:26:30 +0000

    Split move message into turn/advance

--- rpi/router/index.js
+++ rpi/router/index.js
@@ -16,54 +16,52 @@ controller.on("data", (data) => {
 });
 
 comms.on("data", ({ type, data }) => {
-  if (type === "move") {
-    if ("turn" in data) {
-      const DIRECTION_MAP = {
-        left: "L",
-        right: "R",
-      };
+  if (type === "turn") {
+    const DIRECTION_MAP = {
+      left: "L",
+      right: "R",
+    };
 
-      const { turn, direction } = data;
+    const { turn, direction } = data;
 
-      if (typeof turn !== "number") {
-        logger.warn(`Expected turn to be number but got ${typeof turn}`);
-        return;
-      }
-
-      if (!Object.keys(DIRECTION_MAP).includes(direction)) {
-        logger.warn(`Unexpected direction ${direction}`);
-        return;
-      }
-
-      if (turn % 90 != 0) {
-        logger.warn(`Expected turn angle to be multiple of 90 but got ${turn}`);
-        return;
-      }
-
-      // divide by 45 because the robot expects 45 degree multiples
-      robot.send(`${DIRECTION_MAP[direction]}${Math.floor(turn / 45)}`);
-    } else if ("advance" in data) {
-      const DIRECTION_MAP = {
-        forward: "F",
-        backward: "B",
-      };
-
-      const { advance, direction } = data;
-
-      if (typeof advance !== "number" || advance <= 0) {
-        logger.warn(
-          `Expected advance to be positive number but got ${typeof advance}`
-        );
-        return;
-      }
-
-      if (!Object.keys(DIRECTION_MAP).includes(direction)) {
-        logger.warn(`Unexpected direction ${direction}`);
-        return;
-      }
-
-      robot.send(`${DIRECTION_MAP[direction]}${advance}`);
+    if (typeof turn !== "number") {
+      logger.warn(`Expected turn to be number but got ${typeof turn}`);
+      return;
     }
+
+    if (!Object.keys(DIRECTION_MAP).includes(direction)) {
+      logger.warn(`Unexpected direction ${direction}`);
+      return;
+    }
+
+    if (turn % 90 != 0) {
+      logger.warn(`Expected turn angle to be multiple of 90 but got ${turn}`);
+      return;
+    }
+
+    // divide by 45 because the robot expects 45 degree multiples
+    robot.send(`${DIRECTION_MAP[direction]}${Math.floor(turn / 45)}`);
+  } else if (type === "advance") {
+    const DIRECTION_MAP = {
+      forward: "F",
+      backward: "B",
+    };
+
+    const { advance, direction } = data;
+
+    if (typeof advance !== "number" || advance <= 0) {
+      logger.warn(
+        `Expected advance to be positive number but got ${typeof advance}`
+      );
+      return;
+    }
+
+    if (!Object.keys(DIRECTION_MAP).includes(direction)) {
+      logger.warn(`Unexpected direction ${direction}`);
+      return;
+    }
+
+    robot.send(`${DIRECTION_MAP[direction]}${advance}`);
   }
 });
 
