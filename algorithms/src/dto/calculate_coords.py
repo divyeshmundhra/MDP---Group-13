615c7d4ada1297e0227eff5ec89b6fa4532ad4d9
kellypak <kelly_pakxl@yahoo.com.sg>
2021-02-25 15:28:32 +0800

    removed comments

--- algorithms/src/dto/calculate_coords.py
+++ algorithms/src/dto/calculate_coords.py
@@ -7,7 +7,7 @@ class CalculateCoords:
         self.y = coord.get_y()
 
     # directly straight
-    def displacement_straight(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_straight(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.y + displacement < MAP_ROW:
                 return Coord(self.x , self.y + displacement)
@@ -24,7 +24,7 @@ class CalculateCoords:
         return None
     
     # top left tile, moving in the forward direction
-    def displacement_straight_left(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_straight_left(self, displacement: int, orientation: Orientation) -> Coord: 
         if orientation == Orientation.NORTH:
             if 0 <= self.x - 1 < MAP_COL and 0 <= self.y + displacement < MAP_ROW:
                 return Coord(self.x-1 , self.y + displacement)
@@ -41,7 +41,7 @@ class CalculateCoords:
         return None
 
     # top right tile, moving in the forward direction
-    def displacement_straight_right(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_straight_right(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.x+1 < MAP_COL and 0 <= self.y + displacement < MAP_ROW:
                 return Coord(self.x+1, self.y + displacement)
@@ -58,7 +58,7 @@ class CalculateCoords:
         return None
 
     # directly left
-    def displacement_left(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_left(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.x - displacement < MAP_COL:
                 return Coord(self.x - displacement , self.y)
@@ -75,7 +75,7 @@ class CalculateCoords:
         return None
 
     # top left tile, moving in the left direction
-    def displacement_left_up(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_left_up(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.x - displacement-1 < MAP_COL:
                 return Coord(self.x - displacement -1 , self.y)
@@ -92,7 +92,7 @@ class CalculateCoords:
         return None
 
     # bottom left tile, moving in the left direction
-    def displacement_left_down(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_left_down(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.x - displacement < MAP_COL and 0 <= self.y-1 < MAP_ROW:
                 return Coord(self.x - displacement , self.y-1)
@@ -109,7 +109,7 @@ class CalculateCoords:
         return None
 
     # directly right
-    def displacement_right(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_right(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.x + displacement < MAP_COL:
                 return Coord(self.x + displacement, self.y)
@@ -126,7 +126,7 @@ class CalculateCoords:
         return None
 
     # top right tile, moving in the right direction
-    def displacement_right_top(self, displacement: int, orientation: Orientation) -> Coord: # TODO: catch out of bounds error!
+    def displacement_right_top(self, displacement: int, orientation: Orientation) -> Coord:
         if orientation == Orientation.NORTH:
             if 0 <= self.x + displacement < MAP_COL and 0 <= self.y + 1 < MAP_ROW:
                 return Coord(self.x + displacement, self.y+1)
