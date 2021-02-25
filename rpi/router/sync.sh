7af058435c6a1cc66ad63ea8eee613a81793aeff
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-02-25 14:44:12 +0000

    Remove sync script

--- rpi/router/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $# -eq 0 ] ; then
-    echo 'Usage: $0 [ip address]'
-    exit 1
-fi
-
-rsync -rlptzv --progress --exclude=.git --exclude=combined.log --exclude=node_modules "pi@$1:/home/pi/router/" ./
