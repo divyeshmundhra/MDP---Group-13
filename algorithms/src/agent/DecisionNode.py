e5e84b1b62ea9d75a757c7d0bd6303aa36ebb511
Jay <milkweedz@hotmail.com>
2021-03-10 20:53:41 +0800

    decision node should keep track of orientation, fastest path get_full_path method refactored out of get_next_step

--- algorithms/src/agent/DecisionNode.py
+++ algorithms/src/agent/DecisionNode.py
@@ -1,16 +1,21 @@
 from src.dto.coord import Coord
+from src.dto.constants import Orientation
 
 class DecisionNode:
-    def __init__(self, coord: Coord, exact_cost: int, parent):
+    def __init__(self, coord: Coord, orientation: Orientation, exact_cost: int, parent):
         self.coord = coord
+        self.orientation = orientation
         self.exact_cost = exact_cost
         self.parent = parent
-    
+
     def get_coord(self):
         return self.coord
-    
+
+    def get_orientation(self):
+        return self.orientation
+
     def get_exact_cost(self):
         return self.exact_cost
-    
+
     def get_parent(self):
         return self.parent
