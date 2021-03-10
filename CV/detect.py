7aea853555775729c1205ba40e50b92a01e33d68
leeziguang <77472036+leeziguang@users.noreply.github.com>
2021-03-11 00:15:29 +0800

    update to set photo taking conditions

--- CV/detect.py
+++ CV/detect.py
@@ -8,7 +8,7 @@ import torch.backends.cudnn as cudnn
 from numpy import random
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
+from utils.datasets import LoadStreams, LoadImages, LoadImagesZMQ
 from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
     strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
