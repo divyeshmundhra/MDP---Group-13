11c200b584950391dcbf5a3c8166ddac3643bbb3
leeziguang <77472036+leeziguang@users.noreply.github.com>
2021-03-11 00:11:13 +0800

    Add files via upload

--- CV/detect.py
+++ CV/detect.py
@@ -8,21 +8,24 @@ import torch.backends.cudnn as cudnn
 from numpy import random
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages, LoadImagesZMQ
+from utils.datasets import LoadStreams, LoadImages
 from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
     strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
-def detect(save_img=False):
+def detect(save_img=True):
     source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
+    image_id = {'0':'10', '1':'6', '2':'7', '3':'8', '4':'9', '5':'5', '6':'2', '7':'4', '8':'3', '9':'1', '10':'11', '11':'12', '12':'13', '13':'14', '14':'15'}
+    id_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
+
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://'))
 
     # Directories
     save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    save_dir.mkdir(parents=True, exist_ok=True)  # make dir
 
     # Initialize
     set_logging()
@@ -62,6 +65,8 @@ def detect(save_img=False):
     t0 = time.time()
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img) if device.type != 'cpu' else None  # run once
+    num = 0
+    num2 = 0
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -82,6 +87,7 @@ def detect(save_img=False):
             pred = apply_classifier(pred, modelc, img, im0s)
 
         # Process detections
+        
         for i, det in enumerate(pred):  # detections per image
             if webcam:  # batch_size >= 1
                 p, s, im0 = Path(path[i]), '%g: ' % i, im0s[i].copy()
@@ -89,32 +95,47 @@ def detect(save_img=False):
                 p, s, im0 = Path(path), '', im0s
 
             save_path = str(save_dir / p.name)
-            txt_path = str(save_dir / 'labels' / p.stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
+            txt_path = str(save_dir / p.stem) + ('_%g' % dataset.frame if dataset.mode == 'video' else '')
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
+                
+                #if num == 3:                          
+                    #print("finished all detections")
+                    #return
+                    
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+                    s += '%g %s, ' % (n, names[int(c)])  # add to string
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        
+                        bbox_label = ((('%g ' * len(line)).rstrip() % line).split()) # 0:ID, 1:x, 2:y, 3:w, 4:h
                         with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                            if num == 0:
+                                f.write("No. | ID | x | y | width | height"+'\n')
+                            #f.write((str(num)+' %g ' * len(line)).rstrip() % line + '\n')
+                            f.write('%s %s %s %s'%(str(num2), image_id[bbox_label[0]], bbox_label[1], bbox_label[2])+'\n')
 
                     if save_img or view_img:  # Add bbox to image
-                        label = '%s %.2f' % (names[int(cls)], conf)
+                        label = '%s %.2f %f %f' % (image_id[bbox_label[0]], conf, xywh[2], xywh[3])
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+                        if(float(bbox_label[3])>0.2 and float(bbox_label[4])>0.5 and image_id[bbox_label[0]] in id_list): #finetune numbers
+                            cv2.imwrite(save_path+str(num)+".jpg", im0)
+                            id_list.remove(image_id[bbox_label[0]])
+                            num+=1
+                        num2+=1
+                    
 
             # Print time (inference + NMS)
-            print('%sDone. (%.3fs)' % (s, t2 - t1))
+            print('%sNothing detected (%.3fs)' % (s, t2 - t1))
 
             # Stream results
             if view_img:
@@ -123,27 +144,27 @@ def detect(save_img=False):
                     raise StopIteration
 
             # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'images':
-                    cv2.imwrite(save_path, im0)
-                else:
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
+            #if save_img:
+            #    if dataset.mode == 'images':
+            #        cv2.imwrite(save_path+".jpg", im0)
+            #    else:
+            #        if vid_path != save_path:  # new video
+            #            vid_path = save_path
+            #            if isinstance(vid_writer, cv2.VideoWriter):
+            #                vid_writer.release()  # release previous video writer
 
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                    vid_writer.write(im0)
+            #           fourcc = 'mp4v'  # output video codec
+            #            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+            #            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            #            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            #            vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+            #        vid_writer.write(im0)
 
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
+    #if save_txt or save_img:
+    #    s = f"\n{len(list(save_dir.glob('*.txt')))} labels saved to save_dir" if save_txt else ''
+    #    print(f"Results saved to {save_dir}{s}")
 
-    print('Done. (%.3fs)' % (time.time() - t0))
+    #print('Done. (%.3fs)' % (time.time() - t0))
 
 
 if __name__ == '__main__':
