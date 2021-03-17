a6fc00c1944ed3d367af2b703ad2f152f10741c8
Justin Ong <9336475+JustinOng@users.noreply.github.com>
2021-03-18 01:04:57 +0800

    Handle quitting gracefully

--- algorithms/src/agent/LogSimulator.py
+++ algorithms/src/agent/LogSimulator.py
@@ -13,37 +13,43 @@ if len(sys.argv) < 2:
 
 log_fname = sys.argv[1]
 
-pygame.init()
-pygame.display.set_caption('Arena Simulator')
+def main():
+    pygame.init()
+    pygame.display.set_caption('Arena Simulator')
 
-robot_info = RobotInfo(Coord(1, 1), Orientation.NORTH)
-sim_display = SimulationDisplay(robot_info)
+    robot_info = RobotInfo(Coord(1, 1), Orientation.NORTH)
+    sim_display = SimulationDisplay(robot_info)
 
-with open(log_fname, "r") as f:
-    while True:
-        
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    while True:
-                        line = f.readline()
+    with open(log_fname, "r") as f:
+        while True:
+            
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        while True:
+                            line = f.readline()
 
-                        if not line:
-                            break
-
-                        if line:
-                            json_obj = re.search('{.+}', line)
-                            if '"type":"status"' in line:
-                                data = json.loads(json_obj[0])
-                                print(data)
-
-                                _robot_info = data["data"]["robot_info"]
-                                internal_arena = data["data"]["internal_arena"]
-
-                                robot_info.get_coord().set_x(_robot_info["x"])
-                                robot_info.get_coord().set_y(_robot_info["y"])
-                                robot_info.set_orientation(Orientation[_robot_info["orientation"]])
-
-                                sim_display.draw(ArenaStringParser.parse_p1_p2(internal_arena["P1"], internal_arena["P2"]), robot_info)
-                                pygame.display.update()
+                            if not line:
                                 break
+
+                            if line:
+                                json_obj = re.search('{.+}', line)
+                                if '"type":"status"' in line:
+                                    data = json.loads(json_obj[0])
+                                    print(data)
+
+                                    _robot_info = data["data"]["robot_info"]
+                                    internal_arena = data["data"]["internal_arena"]
+
+                                    robot_info.get_coord().set_x(_robot_info["x"])
+                                    robot_info.get_coord().set_y(_robot_info["y"])
+                                    robot_info.set_orientation(Orientation[_robot_info["orientation"]])
+
+                                    sim_display.draw(ArenaStringParser.parse_p1_p2(internal_arena["P1"], internal_arena["P2"]), robot_info)
+                                    pygame.display.update()
+                                    break
+                elif event.type == pygame.QUIT:
+                    return
+
+if __name__ == "__main__":
+    main()
